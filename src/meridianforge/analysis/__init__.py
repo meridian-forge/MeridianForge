c4060d1b6607ebe4a6c3ab7b4458a4d145d396ef
Meridian Forge <teknostrata@gmail.com>
2026-07-22 19:03:48 -0400

    MF-332.6 Finalize analysis model migration

---
+++ src/meridianforge/analysis/__init__.py
@@ -0,0 +1,23 @@
+"""
+Analysis package.
+
+Contains:
+- analysis inputs
+- underwriting compatibility layer
+- analysis utilities
+"""
+
+from meridianforge.analysis.models import (
+    AnalysisInput,
+    Recommendation,
+)
+
+from meridianforge.analysis.underwriting_engine import (
+    UnderwritingEngine,
+)
+
+__all__ = [
+    "AnalysisInput",
+    "Recommendation",
+    "UnderwritingEngine",
+]
